ee1b610238c6be152fd1ff80253c6a2c06332fee
Slav-0N <SOVILGO@gmail.com>
2023-06-28 12:40:37 +0300

    task 2 is ready

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,66 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const unoderedListElement = document.querySelector('ul');
+
+
+const newContainerElem = document.createElement('div');
+
+const addingListElements = ingredients.forEach(ingredient => {
+  const listItemElem = document.createElement("li");
+  listItemElem.classList.add('item');
+  listItemElem.textContent = ingredient;
+
+  newContainerElem.append(listItemElem);
+  
+console.log(unoderedListElement);
+});
+
+const textedElements = newContainerElem.innerHTML;
+console.log(newContainerElem.innerHTML);
+
+unoderedListElement.insertAdjacentHTML('beforebegin', textedElements);
+
+
+
+
+
+
+
+
+
+
+
+
+// const listElement = document.querySelector('#ingredients');
+// console.log(listElement);
+  
+//  const itemStrElem = ingredients.map((ingredient) => {
+//   return `<li class="item">${ingredient}</li>`
+//  });
+
+//  const arrToStringElem = `${itemStrElem.join('')}`
+//  console.log(arrToStringElem);
+
+// const listEl = listElement.insertAdjacentHTML('beforeend', arrToStringElem);
+
+
+
+
+
+
+
+
+  
+  
+  
+
+
+
+
+
+
+
+
