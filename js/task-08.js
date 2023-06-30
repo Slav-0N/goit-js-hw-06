2129ea6df1a46c9a496998366733b572d5405b98
Slav-0N <SOVILGO@gmail.com>
2023-06-30 14:28:38 +0300

    8th Task is ready

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginFormEl = document.querySelector('.login-form');
+
+
+loginFormEl.addEventListener('submit', event => {
+  event.preventDefault();
+  // console.log(event.currentTarget.elements);
+
+  const {elements: {email, password}} = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    window.alert('Всі поля повинні бути заповнені') = alert();
+  };
+
+  const userDataObjEl = {};
+  userDataObjEl.email = email.value;
+  userDataObjEl.password = password.value;
+  console.log(userDataObjEl);
+  event.currentTarget.reset();
+
+});
+
 
