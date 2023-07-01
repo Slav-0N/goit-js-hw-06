8a8c2baebaaa5b48786e86b9b3e4152cebda3d05
Slav-0N <SOVILGO@gmail.com>
2023-07-01 19:11:24 +0300

    6th Task updated
    Виправлено. піся введення невалідного значення бордер залишався червноним.

--- js/task-06.js
+++ js/task-06.js
@@ -2,19 +2,27 @@ const validationInputEl = document.querySelector('#validation-input');
 
 
 
-const changeValidColor = ((event) => {
+const changeValidColor = event => {
   // const validDataEl = validationInputEl.classList.add('valid');
   // const invalidDataEl = validationInputEl.classList.add('invalid');
   const typedSymbolsEl = event.currentTarget.value;
-
-  if (typedSymbolsEl.length === 6) {
-    validationInputEl.classList.add('valid');
-  } else {
+  console.log(typedSymbolsEl);
+  if (typedSymbolsEl.length !== 6) {
+    const typedSymbolsEl = event.currentTarget.value;
+    validationInputEl.classList.remove('valid');
     validationInputEl.classList.add('invalid');
-  }
-
-  
-});
+  } else {
+    validationInputEl.classList.remove('invalid');
+    validationInputEl.classList.add('valid');
+    
+  } 
+};
 
 validationInputEl.addEventListener('blur', changeValidColor);
 
+const clearForm = ev => {
+  console.log(ev.currentTarget.value);
+  ev.currentTarget.value = ''; 
+  
+};
+validationInputEl.addEventListener('click', clearForm);
